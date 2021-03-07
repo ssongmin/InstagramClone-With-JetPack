286d11f81d0f53967f6223c602cebc4768c4f71a
SongMin <96s0907@naver.com>
2021-03-07 14:15:27 +0900

    firstCommit

---
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.instagramclone.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+
+        var navHostFramgnet = supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+
+        var navController = navHostFramgnet.navController
+
+        NavigationUI.setupWithNavController(mainBinding.mainBottomNav, navController)
+    }
+}
